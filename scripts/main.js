d531748d6c9b4e37da1f0803dff417ed5b7d39e4
RushieWashie <57391931+JiroCab@users.noreply.github.com>
2026-05-29 23:40:16 +0800

    Fixed a bug on retry the timer is still 1s, needing to clear save

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
+let prev = -1;
+
+Events.on(GameOverEvent.class, l =>{
+    if(prev > 0) Vars.state.rules.waveSpacing = prev;
+}
+
+Events.on(WaveEvent, l =>{
+    //Just in case, put back the old wave time on last wave
+    if(Vars.state.wave >= Var.sstate.rules.winWave){
+        if(prev > 0) Vars.state.rules.waveSpacing = prev;
+    }
+}
+
 
 Events.on(EventType.WorldLoadEvent, l =>{
-     //delayed so it doesnt overdide initialWaveSpacing
-     Time.run(0.5 * Time.toSeconds, () =>{
+    //delayed so it doesnt overdide initialWaveSpacing
+    Time.run(0.5 * Time.toSeconds, () =>{
+        //Save the "normal" wave delay so it doesnt instantly spawn them all on restart
+        prev = Vars.state.rules.waveSpacing;
+
         if(Vars.state.rules.attackMode){
             Vars.state.rules.waveSpacing = Math.min(Time.toSeconds * 30, Vars.state.rules.waveSpacing);
         }
         //not instant so it doesnt break spawning too hard
         else Vars.state.rules.waveSpacing = 1;
-   
     });
 });
 
